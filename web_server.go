a094b705bc9e3f4a68bedea686c6b84b1bc7eab7
Tom Romeo <Tom.Romeo.Doil@gmail.com>
2022-02-01 20:17:40 +0100

    Refactoring and config.yml
    - added a config.yml that get's parsed in the beginning
    - comments can now be disabled, enabling a pure file serving mode
    - restructured the code -> split up the one big handle method

--- web_server.go
+++ web_server.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	//"fmt"
 	"database/sql"
+	"gopkg.in/yaml.v3"
 	"html/template"
 	"io/ioutil"
 	"log"
@@ -13,7 +13,6 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/russross/blackfriday"
 )
-
 
 type Post struct {
 	Status   string
@@ -29,27 +28,75 @@ type Comment struct {
 	Name, Comment string
 }
 
-var db *sql.DB
+var (
+	db   *sql.DB
+	conf Conf
+)
+
+type Conf struct {
+	AllowComments bool `yaml:"allow_comments"`
+}
+
+func parseConfig() {
+	yConfig, err := ioutil.ReadFile("config.yml")
+	if err != nil {
+		log.Fatalf("Could not open config.yml: %s", err)
+	}
+	if err := yaml.Unmarshal(yConfig, &conf); err != nil {
+		log.Fatalf("Could not parse config.yml: %s", err)
+	}
+}
 
 func init() {
-	// you do not have to open the db connection on every request
-	// it can be done once at the start of the app
-	var err error
-	db, err = sql.Open("mysql", "username:password(localhost:3306)/databasename")
-	if err != nil {
-		log.Fatal(err)
+	parseConfig()
+
+	//only connect to sqlite if we want comment functionality
+	if conf.AllowComments {
+
+		// you do not have to open the db connection on every request
+		// it can be done once at the start of the app
+		db, err := sql.Open("mysql", "username:password(localhost:3306)/databasename")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// Open doesn't open a connection. Validate DSN data:
+		err = db.Ping()
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
-	// Open doesn't open a connection. Validate DSN data:
-	err = db.Ping()
-	if err != nil {
-		log.Fatal(err)
+}
+
+func handlerequest(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	switch r.Method {
+	case http.MethodGet:
+		handleIndex(w, r)
+	case http.MethodPost:
+		handlePostComment(w, r)
 	}
 
 }
 
-func handlerequest(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "POST" {
+func handleIndex(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path[1:] == "" {
+		posts := getPosts()
+		t := template.New("index.html")
+		t, err := t.ParseFiles("index.html")
+		if err != nil {
+			log.Fatal(err)
+		}
+		if err := t.Execute(w, posts); err != nil {
+			log.Fatal(err)
+		}
+		return
+	}
+}
 
+func handlePostComment(w http.ResponseWriter, r *http.Request) {
+
+	if conf.AllowComments {
 		uniquepost := r.FormValue("uniquepost")
 		namein := r.FormValue("name")
 		commentin := r.FormValue("comment")
@@ -67,40 +114,39 @@ func handlerequest(w http.ResponseWriter, r *http.Request) {
 		http.Redirect(w, r, r.URL.Path, 301)
 		return
 	}
+}
 
-	if r.URL.Path[1:] == "" {
-		posts := getPosts()
-		t := template.New("index.html")
-		t, _ = t.ParseFiles("index.html")
-		t.Execute(w, posts)
-		return
-	}
-	uniquepost := r.URL.Path[1:]
-	rows, err := db.Query("select id, name, comment from comments where uniquepost = ?", uniquepost)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer rows.Close()
+func handlePosts(w http.ResponseWriter, r *http.Request) {
+	uniquepost := strings.Replace(r.URL.Path, "/posts/", "", -1)
 
-	//declear an array to keep all comments
+	// declare an array to keep all comments
 	var comments []Comment
 
-	for rows.Next() {
-		var id int
-		var name, comment string
-		err := rows.Scan(&id, &name, &comment)
+	if conf.AllowComments {
+
+		rows, err := db.Query("select id, name, comment from comments where uniquepost = ?", uniquepost)
 		if err != nil {
 			log.Fatal(err)
 		}
-		//append the comment into the array when done
-		comments = append(comments, Comment{name, comment})
-	}
-	err = rows.Err()
-	if err != nil {
-		log.Fatal(err)
+		defer rows.Close()
+
+		for rows.Next() {
+			var id int
+			var name, comment string
+			err := rows.Scan(&id, &name, &comment)
+			if err != nil {
+				log.Fatal(err)
+			}
+			//append the comment into the array when done
+			comments = append(comments, Comment{name, comment})
+		}
+		err = rows.Err()
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
 
-	f := "posts/" + r.URL.Path[1:] + ".md"
+	f := "posts/" + uniquepost + ".md"
 	fileread, _ := ioutil.ReadFile(f)
 	lines := strings.Split(string(fileread), "\n")
 	status := string(lines[0])
@@ -109,7 +155,8 @@ func handlerequest(w http.ResponseWriter, r *http.Request) {
 	summary := string(lines[3])
 	body := strings.Join(lines[4:len(lines)], "\n")
 	htmlBody := template.HTML(blackfriday.MarkdownCommon([]byte(body)))
-	post := Post{status,title, date, summary, htmlBody, r.URL.Path[1:], comments}
+
+	post := Post{status, title, date, summary, htmlBody, uniquepost, comments}
 	t := template.New("post.html")
 	t, _ = t.ParseFiles("post.html")
 	t.Execute(w, post)
@@ -122,6 +169,9 @@ func getPosts() []Post {
 	for _, f := range files {
 		file := strings.Replace(f, "posts/", "", -1)
 		file = strings.Replace(file, ".md", "", -1)
+
+		// replace backticks for windows use
+		file = strings.Replace(file, "posts\\", "", -1)
 		fileread, _ := ioutil.ReadFile(f)
 		lines := strings.Split(string(fileread), "\n")
 		status := string(lines[0])
@@ -137,10 +187,14 @@ func getPosts() []Post {
 }
 
 func main() {
-http.HandleFunc("/", handlerequest)
-http.Handle("/css/", http.StripPrefix("/css", http.FileServer(http.Dir("/location/onyourserver/css"))))
-http.Handle("/js/", http.StripPrefix("/js", http.FileServer(http.Dir("/location/onyourserver/js"))))
+	http.HandleFunc("/", handlerequest)
+	http.HandleFunc("/posts/", handlePosts)
+	http.Handle("/css/", http.StripPrefix("/css", http.FileServer(http.Dir("./content/css"))))
+	http.Handle("/js/", http.StripPrefix("/js", http.FileServer(http.Dir("./content/js"))))
 
-	http.ListenAndServe(":8000", nil)
+	log.Println("Blog Server deployed on :8000")
+	if err := http.ListenAndServe(":8000", nil); err != nil {
+		log.Fatal(err)
+	}
 
 }
